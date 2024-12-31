39fa475eb36c7a13875845d8e9315d06d01dd92e
LightlyToastedAngel <unodosgametime@gmail.com>
2024-12-31 15:21:14 +0100

    Initial commit

---
+++ src/main/java/net/toast/dyefulmod/DyefulClient.java
@@ -0,0 +1,10 @@
+package net.toast.dyefulmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DyefulClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
